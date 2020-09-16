cf346b6fddce0fd963e3ec771201537ca5355099
Nzparra <nzparra@gmail.com>
2020-09-16 10:52:49 -0500

    [FIX]Missing values

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -6,18 +6,27 @@
  **/
 int is_palindrome(unsigned long n)
 {
-	unsigned long i = n, j = 0;
+	unsigned long unit = 1, start, end, temp = n, i = 0;
 
-    if (n < 10)
-        return (1);
-	while (i != 0)
-	{
-		j = j * 10;
-		j = j + (i % 10);
-		i = j / 10;
-	}
-	if (j == n)
+	if (n < 10)
 		return (1);
-	else
-		return (0);
+	while (temp > 9)
+	{
+		unit = unit * 10;
+		temp /= 10;
+		i++;
+	}
+	temp = 0;
+	while (i > temp)
+	{
+		start = n / unit;
+		end = n % 10;
+		if (start != end)
+			return (0);
+		i--;
+		temp++;
+		n = (n % unit) / 10;
+		unit /= 10;
+	}
+	return (1);
 }
