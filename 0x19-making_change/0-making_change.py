285b9ed9a8c5500ffe9d3bff459ae6b8230c5654
Nzparra <nzparra@gmail.com>
2021-02-27 10:51:42 -0500

    ADD
    Making change

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" number of coins """
+
+
+def makeChange(coins, total):
+    """
+    determining how many coins
+    """
+    sum = 0
+    i = 0
+    counter = 0
+    if total <= 0:
+        return 0
+    new_coins = sorted(coins, reverse=True)
+    my_coins = len(new_coins)
+    aux = total - sum
+    while sum < total and i < my_coins:
+        while new_coins[i] <= aux:
+            sum = sum + new_coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+            aux = total - sum
+        i += 1
+    return -1
