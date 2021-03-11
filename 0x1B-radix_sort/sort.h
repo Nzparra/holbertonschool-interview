e2c4629acf90f2165bde43958250caf2dd45ee76
Nzparra <nzparra@gmail.com>
2021-03-10 19:22:44 -0500

    Update sort.h

--- 0x1B-radix_sort/sort.h
+++ 0x1B-radix_sort/sort.h
@@ -1,9 +1,10 @@
-#ifndef ZETA
-#define ZETA
+  #ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 void print_array(const int *array, size_t size);
 void radix_sort(int *array, size_t size);
-#endif
+
+#endif /* SORT_H */
